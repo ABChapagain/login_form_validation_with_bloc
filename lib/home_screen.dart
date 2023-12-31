8736f940603db8f53f484cdaa402dce69efd2286
abchapagain <achyutchapagain05@gmail.com>
2023-12-31 20:54:25 +0545

    done done done

---
+++ lib/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login_form_validation/bloc/auth_bloc.dart';
+import 'package:login_form_validation/login_screen.dart';
+import 'package:login_form_validation/widgets/gradient_button.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthInitial) {
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => const LoginScreen()),
+              (route) => false,
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text((state as AuthSuccess).uid),
+              const SizedBox(height: 20),
+              GradientButton(
+                text: 'Logout',
+                onPressed: () {
+                  context.read<AuthBloc>().add(AuthLogoutRequested());
+                },
+              ),
+            ],
+          ));
+        },
+      ),
+    );
+  }
+}
